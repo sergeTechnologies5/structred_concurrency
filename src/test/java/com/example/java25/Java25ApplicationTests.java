ae44b3ce9449b0ecd1875ae985d2b22f61ae0737
Nthiga Phineas <pnthiga@dtbafrica.com>
2025-09-27 10:20:25 +0300

    Initial Running Commit

---
+++ src/test/java/com/example/java25/Java25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
